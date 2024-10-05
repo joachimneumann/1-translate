2587699f1f4fa5faa73b6c2cd39a6923564962f3
Joachim Neumann <joachim@joachimneumann.com>
2024-10-05 19:03:02 +0700

    improved Key animations

--- Source/ViewModel.swift
+++ Source/ViewModel.swift
@@ -22,15 +22,10 @@ import SwiftGmp
     private static let MAX_DISPLAY_LEN = 10_000 /// too long strings in Text() crash the app
     private let keyColor = KeyColor()
 
-    private var upHasHappended = false
-    private var downAnimationFinished = false
+    private var upHasHappended: [String: Bool] = [:]
+    private var downAnimationFinished: [String: Bool] = [:]
     
-    private enum KeyState {
-        case notPressed
-        case pressed
-    }
-    
-    private var keyState: KeyState = .notPressed //{ didSet { print("keyState ->", keyState) } }
+
     private let downTime = 0.1
     private let upTime = 0.4
     private var previouslyPendingOperator: String? = nil
@@ -112,25 +107,7 @@ import SwiftGmp
             }
         }
     }
-    
-    func showDownColors(for symbol: String) async {
-        upHasHappended = false
-        downAnimationFinished = false
-        await MainActor.run {
-            withAnimation(.easeIn(duration: downTime)) {
-                keyStatusColor[symbol] = keyColor.downColor(for: symbol, isPending: symbol == previouslyPendingOperator)
-            }
-        }
-        //print("down: downColor sleep START", downTime)
-        try? await Task.sleep(nanoseconds: UInt64(downTime * 1_000_000_000))
-        //print("down: downColor sleep STOP")
-        downAnimationFinished = true
-        //print("down: upHasHappended", upHasHappended)
-        if upHasHappended {
-            await showUpColors(for: symbol)
-        }
-    }
-    
+
     func showUpColors(for symbol: String) {
         /// Set the background color back to normal
         withAnimation(.easeIn(duration: upTime)) {
@@ -139,24 +116,25 @@ import SwiftGmp
     }
     
     func touchDown(for symbol: String) {
-        Task(priority: .userInitiated) {
-            await showDownColors(for: symbol)
+        upHasHappended[symbol] = false
+        downAnimationFinished[symbol] = false
+        withAnimation(.easeIn(duration: downTime)) {
+            keyStatusColor[symbol] = keyColor.downColor(for: symbol, isPending: symbol == previouslyPendingOperator)
         }
-//        Task(priority: .userInitiated) {
-//            let validOrAllowed = displayNumber.isValid || !keysThatRequireValidNumber.contains(symbol)
-//            //print("touchDown2 keyState =", keyState, "validOrAllowed =", validOrAllowed)
-//            guard keyState == .notPressed && validOrAllowed else {
-//                //keyState = .disabledPressed
-//                await showDisabledColors(for: symbol)
-//                return
-//            }
-//        }
+        Task(priority: .userInitiated) {
+            try? await Task.sleep(nanoseconds: UInt64(downTime * 1_000_000_000))
+            downAnimationFinished[symbol] = true
+            if upHasHappended[symbol] ?? false {
+                showUpColors(for: symbol)
+            }
+        }
     }
     
     func touchUp(of symbol: String, screen: Screen) {
-        showUpColors(for: symbol)
-        keyState = .notPressed
-        showUpColors(for: symbol)
+        upHasHappended[symbol] = true
+        if downAnimationFinished[symbol] ?? false {
+            showUpColors(for: symbol)
+        }
     }
     
 //    func setPendingColors(for symbol: String) async {
@@ -182,7 +160,6 @@ import SwiftGmp
 //    }
     
 //    func defaultTask(for symbol: String, screen: Screen) {
-//        keyState = .notPressed
 //        calculator.asResult(symbol)
 //        refreshDisplay(screen: screen)
 //        lastScreen = screen
